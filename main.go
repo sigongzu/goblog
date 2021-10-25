66e6856628c31a33e3e463a698ce19439b739ba5
lianglizheng <llzzjy@gmail.com>
2021-10-25 17:03:03 +0800

    新增关于页面

--- main.go
+++ main.go
@@ -6,7 +6,14 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, goblog</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello, goblog homepage</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+			"<a href=\"mailto:sigongzu@163.com\">sigongzu@163.com</a>")
+	} else {
+		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>")
+	}
 }
 
 func main() {
