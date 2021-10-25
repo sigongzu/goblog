9f38e382ce17e813346253d541b5e24ab207f4c3
lianglizheng <llzzjy@gmail.com>
2021-10-25 17:31:16 +0800

    404 状态码

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
 			"<a href=\"mailto:sigongzu@163.com\">sigongzu@163.com</a>")
 	} else {
+		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>")
 	}
 }
