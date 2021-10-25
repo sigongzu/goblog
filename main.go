e461d9f818fda5b386a26b9af9daa3ac90cb18f2
lianglizheng <llzzjy@gmail.com>
2021-10-25 22:28:05 +0800

    移除URL路径中的斜杠

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -42,6 +43,16 @@ func forceHTMLMiddleware(next http.Handler) http.Handler {
 	})
 }
 
+func removeTrailingSlash(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 
 	router := mux.NewRouter()
@@ -62,5 +73,5 @@ func main() {
 	// articleURL, _ := router.Get("articles.show").URL("id", "23")
 	// fmt.Println("articleURL: ", articleURL)
 
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", removeTrailingSlash(router))
 }
